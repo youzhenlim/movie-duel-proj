d0098361af39114bc8e11bcacef9ebffc3d44e9c
youzhenlim <limyouzhen@gmail.com>
2023-06-09 12:40:42 +0800

    created async func to fetch movies

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'e9bacc05',
+            s: 'avengers'
+    }});
+
+    console.log(response.data);
+};
+
+fetchData();
