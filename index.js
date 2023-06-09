e43da8ed410c4eb90f130b0fff57617bcde911da
youzhenlim <limyouzhen@gmail.com>
2023-06-09 18:46:14 +0800

    parsed statistics for movie details

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ createAutoComplete({
     root : document.querySelector('#left-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     }
 });
 
@@ -42,12 +42,15 @@ createAutoComplete({
     root : document.querySelector('#right-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-summary'));
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
     }
 });
 
+let leftMovie;
+let rightMovie;
 
-const onMovieSelect = async (movie, summaryElement) => {
+
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get("http://www.omdbapi.com/", {
         params: {
           apikey: "e9bacc05",
@@ -56,9 +59,43 @@ const onMovieSelect = async (movie, summaryElement) => {
       });
 
       summaryElement.innerHTML = movieTemplate(response.data)
-}
+
+      if(side === 'left'){
+        leftMovie = response.data
+      }
+
+      else{
+        rightMovie = response.data
+      }
+
+      if (leftMovie && rightMovie){
+        runComparison();
+      }
+};
+
+const runComparison = () => {
+    console.log('Time for comparison');
+};
 
 const movieTemplate = (movieDetail) => {
+
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''))
+    const metascore = parseInt(movieDetail.Metascore)
+    const imdbRating = parseFloat(movieDetail.imdbRating)
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''))
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) =>{
+
+        const value = parseInt(word)
+        
+        if(isNaN(value)){
+            return prev;
+        } else{
+            return prev + value;
+        }
+    }, 0);
+
+    console.log(awards);
+
     return `
         <article class="media">
             <figure class="media-left">
