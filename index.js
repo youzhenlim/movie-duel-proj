554bc7b72e44f91f7787ff6f9bde5cf5e055444a
youzhenlim <limyouzhen@gmail.com>
2023-06-09 12:57:51 +0800

    created user input search request function

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: 'e9bacc05',
-            s: 'avengers'
+            s: searchTerm
     }});
 
     console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = (event) => {
+
+    if(timeoutId){
+        clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(()=>{
+
+        fetchData(event.target.value)
+    
+    }, 1000);
+    
+};
+
+input.addEventListener('input', onInput);
