956aa7b22d5735ea06d88e9c3c41706daaad56fd
youzhenlim <limyouzhen@gmail.com>
2023-06-09 13:34:40 +0800

    rendered movies with poster and title

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data);
+  if(response.data.Error){
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
 
 
-const onInput = debounce(event => {
-  fetchData(event.target.value);
+const onInput = debounce( async (event) => {
+  const movies = await fetchData(event.target.value);
+  
+  for (let movie of movies){
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <img src = "${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+    `
+
+    document.querySelector('#target').appendChild(div);
+  }
+
 }, 1000);
+
 
 input.addEventListener("input", onInput);
